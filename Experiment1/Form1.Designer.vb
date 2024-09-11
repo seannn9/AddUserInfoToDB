905b764de775a547eaec02b1dae64537ba90623c
seannn9 <naeskirlu9@gmail.com>
2024-09-11 14:06:55 +0800

    disabled resizing

--- Experiment1/Form1.Designer.vb
+++ Experiment1/Form1.Designer.vb
@@ -261,6 +261,8 @@ Partial Class Form1
         Me.Controls.Add(Me.Panel2)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(465, 448)
+        Me.MinimumSize = New System.Drawing.Size(465, 448)
         Me.Name = "Form1"
         Me.Text = "T-CPET411LA Experiment 1"
         Me.Panel1.ResumeLayout(False)
